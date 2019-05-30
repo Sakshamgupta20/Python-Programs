b378042036502bf9beaa3a4881051fff2a0fc1b6
Sakshamgupta20 <sg20saksham@gmail.com>
2019-05-30 13:40:23 +0530

    Validating uid

---
+++ Validating UID.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 30 12:23:26 2019
+
+@author: Saksham
+"""
+
+import re
+
+for _ in range(int(input())):
+    u = ''.join(sorted(input()))
+    try:
+        assert re.search(r'[A-Z]{2}', u)
+        assert re.search(r'\d\d\d', u)
+        assert not re.search(r'[^a-zA-Z0-9]', u)
+        assert not re.search(r'(.)\1', u)
+        assert len(u) == 10
+    except:
+        print('Invalid')
+    else:
+        print('Valid')
